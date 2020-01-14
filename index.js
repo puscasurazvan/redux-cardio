25645ce52bdfc4db63163da23d2eacfc12d08275
Razvan Puscasu <puscasurazvan93@gmail.com>
2020-01-14 16:48:20 +0000

    aha moment with thunks

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const redux = require('redux')
+const reduxLogger = require('redux-logger')
+
 const createStore = redux.createStore
+const combineReducers = redux.combineReducers
+const applyMiddleware = redux.applyMiddleware
+const logger = reduxLogger.createLogger()
 
 const BUY_CAKE = 'BUY_CAKE'
 const BUY_ICECREAM = 'BUY_ICECREAM'
@@ -11,36 +16,51 @@ function buyCake() {
   }
 }
 
-// (previousState, action) => newState
-
-const initialState = {
-  numberOfCakes: 10,
-  numberOfIceCreams: 20
+function buyIceCream() {
+  return {
+    type: BUY_ICECREAM,
+    info: 'First redux action'
+  }
 }
 
-const reducer = (state = initialState , action) => {
+const initialCakeState = {
+  numberOfCakes: 10,
+}
+
+const initialIceCreamState = {
+  numberOfIceCreams: 10,
+}
+
+const cakeReducer = (state = initialCakeState , action) => {
   switch (action.type) {
     case BUY_CAKE: return {
       ...state,
       numberOfCakes: state.numberOfCakes - 1
     }
 
+    default: return state
+  }
+}
+
+const iceCreamReducer = (state = initialIceCreamState , action) => {
+  switch (action.type) {
     case BUY_ICECREAM: return {
       ...state,
-      numberOfCakes: state.numberOfIceCreams - 1
+      numberOfIceCreams: state.numberOfIceCreams - 1
     }
 
     default: return state
   }
 }
 
-const store = createStore(reducer)
-
-console.log('initialstate', store.getState())
-
-const unsubscribe = store.subscribe(() => {
-  console.log('Updated state', store.getState())
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer 
 })
+
+const store = createStore(rootReducer, applyMiddleware(logger))
+
+const unsubscribe = store.subscribe(() => {})
 
 store.dispatch(buyCake())
 store.dispatch(buyCake())
