ef5454ef24fbe0de91d505ee03f9d7f8a9582533
Razvan Puscasu <puscasurazvan93@gmail.com>
2020-01-15 13:52:40 +0000

    shop icecream

--- Sweet-Shop/src/App.js
+++ Sweet-Shop/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import CakeContainer from './components/CakeContainer'
 import HooksCakeContainer from './components/HooksCakeContainer'
+import IceCreamContainer from './components/IceCreamContainer'
 import { Provider } from 'react-redux'
 import store from './redux/store'
 
@@ -10,6 +11,7 @@ function App() {
       <div className="App">
         <CakeContainer />
         <HooksCakeContainer />
+        <IceCreamContainer  />
       </div>
     </Provider>
   )
